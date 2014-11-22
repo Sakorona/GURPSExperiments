f7f52908831ded8f71a0ba82abe81166670461e1
N Johnson <johnson.nicholas.h@gmail.com>
2014-11-22 02:10:54 -0800

    - did not properly include a file, causing all test builds to fail.

---
+++ TwilightShards.GURPSUtil/Moon Classes/MoonRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwilightShards.GURPSUtil
+{
+    public class MoonRecord
+    {
+        public MoonType moon {get; set;}
+        public double orbitalRadius { get; set; }
+        public WorldSize moonSize { get; set; }
+
+        public MoonRecord(MoonType m, double d, WorldSize s)
+        {
+            moon = m;
+            orbitalRadius = d;
+            moonSize = s;
+        }
+    }
+}
